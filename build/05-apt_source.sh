124dfaec398cb7fb7e9dc4d5bc24bdb0f971e082
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-07-31 14:46:25 -0500

    Update 05-apt_source.sh

--- build/05-apt_source.sh
+++ build/05-apt_source.sh
@@ -1,1 +1,7 @@
+#Adding Unifi Repository to apt-source list:
+echo 'deb [ arch=amd64,arm64 ] https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 
+#Adding apt-get update secure gpg key to use repository
+#Use both methods form https://help.ui.com/hc/en-us/articles/220066768-Updating-and-Installing-Self-Hosted-UniFi-Network-Servers-Linux
+sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
