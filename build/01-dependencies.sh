71691adb3bdaf9739effc7819a2d0b5df628be4a
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-07-31 14:11:40 -0500

    Update 01-dependencies.sh
    Initial Dependencies only.
    Things like vim, nano, bindutils(dig)... not application and configuration specific...

--- build/01-dependencies.sh
+++ build/01-dependencies.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 apt-get update
-apt-get -y install wget curl nano gnupg cron logrotate 
-#will neeed to recreate to grab all necessary files...
+apt-get -y install wget curl nano gnupg cron
