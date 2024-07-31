ea00e02d09301a2cad4a19f4b80d42e0cc66ca7d
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-07-31 13:09:09 -0500

    Update 01-dependencies.sh

--- build/01-dependencies.sh
+++ build/01-dependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 apt-get update
-apt-get -y install wget curl nano gnupg cron
+apt-get -y install wget curl nano gnupg cron logrotate 
+#will neeed to recreate to grab all necessary files...
