580d41208a13568dcc7b24a4af122207cc8ffaac
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-08-02 01:00:57 -0500

    Update 05-apt_source.sh

--- build/05-apt_source.sh
+++ build/05-apt_source.sh
@@ -1,12 +1,17 @@
+
+#Use both methods from https://help.ui.com/hc/en-us/articles/220066768-Updating-and-Installing-Self-Hosted-UniFi-Network-Servers-Linux
+
 #Adding Unifi Repository to apt-source list:
 echo 'deb [ arch=amd64,arm64 ] https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 
-#Adding apt-get update secure gpg key to use repository
-#Use both methods form https://help.ui.com/hc/en-us/articles/220066768-Updating-and-Installing-Self-Hosted-UniFi-Network-Servers-Linux
+#Adding apt-get update secure gpg key to use repository:
 sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
+
+#Confirm ubquity key:
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
 
-#Confirm reprop
+#Confirm repro
 sudo apt-get update
-#Confirm Lattest unifi
+
+#Confirm Lattest unifi is installed
 sudo apt-get install unifi
