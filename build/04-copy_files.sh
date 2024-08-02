f8e18f0b6cce71df7f392be561753250bd0455f5
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-08-02 00:54:40 -0500

    Update 04-copy_files.sh

--- build/04-copy_files.sh
+++ build/04-copy_files.sh
@@ -2,3 +2,4 @@
 cd /tmp/build
 chmod 755 $(ls -1 /tmp/build/ | grep -v "^[0-9][0-9]-")
 cp /tmp/build/update-unifi.sh /root/update-unifi.sh
+cp /tmp/build/update-system.sh /root/full-system-update.sh
