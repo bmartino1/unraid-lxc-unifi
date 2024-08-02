f029ce491b9d3e801db8b7145bd32c5205c64740
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-08-02 01:14:37 -0500

    Update 03-unifi.sh

--- build/03-unifi.sh
+++ build/03-unifi.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+#Unifi Grab version
 LAT_V="$(wget -qO- https://dl.ui.com/unifi/debian/dists/stable/ubiquiti/binary-amd64/Packages | grep "^Version:" | awk '{print $2}' | cut -d '-' -f1)"
 wget -O /tmp/unifi.deb "https://dl.ui.com/unifi/${LAT_V}/unifi_sysvinit_all.deb"
+
+#Install unifi using app over dokg -i...
 apt-get -y install /tmp/unifi.deb
+
+#Clean up
 rm -f /tmp/unifi.deb
 systemctl stop unifi
+
+#Hostname set for unifi device adoption.
 hostnamectl set-hostname unifi
+
+echo "Completed Main Service Install of Unifi Network Applicaiton"
