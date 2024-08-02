359b0782da7b0b589d028d46d294ed504fe49867
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-08-02 00:56:18 -0500

    Update update-system.sh

--- build/update-system.sh
+++ build/update-system.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #Check and update system
 echo "System check and Full system upgrade"
-apt update && apt full-upgrade -y && do-release-upgrade
+apt update && apt full-upgrade -y
+do-release-upgrade -d -f DistUpgradeViewNonInteractive
