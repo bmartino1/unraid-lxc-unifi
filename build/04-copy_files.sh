6d28b349f904537b54fd636d556b4f85440526d2
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-08-02 01:16:05 -0500

    Update 04-copy_files.sh

--- build/04-copy_files.sh
+++ build/04-copy_files.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+#Fix Permission of files in build not container a number...
 cd /tmp/build
 chmod 755 $(ls -1 /tmp/build/ | grep -v "^[0-9][0-9]-")
+
+#copy scripts to /root for cron and end users
 cp /tmp/build/update-unifi.sh /root/update-unifi.sh
 cp /tmp/build/update-system.sh /root/full-system-update.sh
+
+echo "Completed File Copy"
