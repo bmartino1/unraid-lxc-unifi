91258959b52543edc70948e3893aa3b2763c1e9b
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-08-02 01:21:53 -0500

    Update 99-cleanup.sh

--- build/99-cleanup.sh
+++ build/99-cleanup.sh
@@ -3,3 +3,5 @@ apt clean
 rm -rf /var/cache/apt/archives/*
 rm -f /etc/ssh/*_key /etc/ssh/*.pub
 find /var/log -type f -mtime +30 -exec rm {} \;
+
+echo "LXC Security Cleanup to help reduce file size and Secure at Build generated SSH keys"
