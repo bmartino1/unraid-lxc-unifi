e3e92128d492bed73693baff3cbcbff441db01ec
bmartino1 <167354702+bmartino1@users.noreply.github.com>
2024-08-03 19:01:28 -0500

    Update 06-auto-update-system.sh

--- build/06-auto-update-system.sh
+++ build/06-auto-update-system.sh
@@ -2,13 +2,13 @@
 
 # Install unattended-upgrades
 #sudo apt update
-#sudo apt install -y unattended-upgrades
+sudo apt install -y unattended-upgrades
 
 # Pre-configure unattended-upgrades
-#echo "unattended-upgrades unattended-upgrades/enable_auto_updates boolean true" | sudo debconf-set-selections
+echo "unattended-upgrades unattended-upgrades/enable_auto_updates boolean true" | sudo debconf-set-selections
 
 # Reconfigure unattended-upgrades to apply the pre-configured setting
-#sudo dpkg-reconfigure --frontend=noninteractive unattended-upgrades
+sudo dpkg-reconfigure --frontend=noninteractive unattended-upgrades
 
 #echo "Unattended-upgrades has been installed and configured to automatically download and install stable updates."
 
