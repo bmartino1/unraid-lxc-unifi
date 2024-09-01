64708ae3e9d4c86651b2062c8208c823d68221aa
Brandon Martino <167354702+bmartino1@users.noreply.github.com>
2024-09-01 12:43:35 -0500

    Update update-system.sh
    update end user script.

--- build/update-system.sh
+++ build/update-system.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #Check and update system
+echo "update unifi repo for release changes"
+apt-get update --allow-releaseinfo-change
+apt-get update --allow-releaseinfo-change
+apt-get update --allow-releaseinfo-change
+
 echo "System check and Full system upgrade"
 apt update && apt full-upgrade -y
 do-release-upgrade -d -f DistUpgradeViewNonInteractive
